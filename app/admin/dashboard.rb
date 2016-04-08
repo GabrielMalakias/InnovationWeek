16b81666d5d0bafe35c9e7839884ab458d50345a
Gabriel Souza <gabriel.souza@locaweb.com.br>
2016-04-08 18:39:32 -0300

    Start dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,33 +4,38 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     columns do
-       column do
-         panel "Analistas" do
-           #table_for Doodle::User::Analyst.joins(:user_channels).where('doodle_user_channels.status' => 'online') do
-           panel "Ativos" do
-             table_for [ {login: 'aladdin', protocols_number: 17} ] do
-               column :login
-               column :protocols_number
-             end
-           end
-           panel "Em Espera" do
-             table_for [ {login: 'aladdin'} ] do
-               column :login
-             end
+      column do
+        panel "Analistas" do
+          #table_for Doodle::User::Analyst.joins(:user_channels).where('doodle_user_channels.status' => 'online') do
+          panel "Ativos" do
+            table_for [ {login: 'aladdin', protocols_number: 17} ] do
+              column :login
+              column :protocols_number
             end
-         end
-       end
+          end
+          panel "Em Espera" do
+            table_for [ {login: 'aladdin'} ] do
+              column :login
+            end
+          end
+        end
+      end
 
-       column do
-         panel "Número de protocolos em cada fila" do
-
-         end
-       end
-
-       column do
-         panel "Número de protocolos por status" do
-         end
-       end
-     end
+      column do
+        panel 'Protocolos' do
+          panel  "Waiting"
+          table_for [ {name: 'corporativo', number: 17} ] do
+            column :name
+            column :number
+          end
+        end
+        panel  "Doing" do
+          table_for [ {name: 'corporativo', number: 17} ] do
+            column :name
+            column :number
+          end
+        end
+      end
+    end
   end
 end
